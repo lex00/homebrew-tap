8a795fcae2d3fbadf7c8ff5105c261c0a647c31a
lex00 <121451605+lex00@users.noreply.github.com>
2026-01-25 17:16:03 -0700

    Update chant to v0.1.3

--- Formula/chant.rb
+++ Formula/chant.rb
@@ -1,22 +1,22 @@
 class Chant < Formula
   desc "Intent Driven Development - specification-driven development tool"
   homepage "https://github.com/lex00/chant"
-  version "0.1.1"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/lex00/chant/releases/download/v#{version}/chant-macos-aarch64"
-      sha256 "1ca3b4fbe7c5284f373765578c6e800d3b17e6655c3c4c758fdf01ed96b5ebf5"
+      sha256 "f793358a21dc4339bc4f716df65f65474973510fe55146ab89b34bc3146655e8"
     else
       url "https://github.com/lex00/chant/releases/download/v#{version}/chant-macos-x86_64"
-      sha256 "dd8020cc5f2999a10fbaf733c591c047942cd06f89149d3a8a486b77d2f34f48"
+      sha256 "1424496b5f456a9d495d4e828bea3f1ec6b153cd562e87462f655686a5b2d05a"
     end
   end
 
   on_linux do
     url "https://github.com/lex00/chant/releases/download/v#{version}/chant-linux-x86_64"
-    sha256 "c3e857c4ce135c65426f57fa980432e81d005dab2645db53442322f68df1b43a"
+    sha256 "77a052e28b63ffc142a74030a50f032ab987ca158895bd785c6b4b827e115b21"
   end
 
   def install
